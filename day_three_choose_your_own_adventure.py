32fdf5d5b6299354e2f78091687cacebe2e99784
Jennifer Baughman <jbaughman@protocolzero.com>
2022-01-29 14:20:00 -0600

    Begin day three: choose your own adventure. Began setting up function stubs.

---
+++ day_three_choose_your_own_adventure.py
@@ -0,0 +1,19 @@
+"""
+day_three_choose_your_own_adventure
+Created January 29, 2022 by Jennifer Baughman
+
+Description: Create a small choose your own adventure game!
+"""
+
+
+def parse_commands(command):
+    valid_cmds = ("left", "right", "wait", "swim", "red", "yellow", "blue",)
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
