bd987b64b6914b86abf0b2e5a938669118afadab
Jennifer Baughman <jbaughman@corelogic.com>
2022-01-29 13:15:51 -0600

    Initial commit. Files for Udemy's 100 Days of Code (Python). Includes gitignore and day one file.

---
+++ day_one.py
@@ -0,0 +1,18 @@
+"""
+day_one
+Created January 28, 2022 by Jennifer Baughman
+
+Description: Band Name Generator program
+Day one of Udemy #100 Days of Code
+"""
+
+
+def main():
+    print("Welcome to the Band Name Generator.")
+    city_name = input("What's the name of the city you grew up in?")
+    pet_name = input("What's your pet's name?")
+    print(f"Your band name could be {city_name} {pet_name}")
+
+
+if __name__ == "__main__":
+    main()
