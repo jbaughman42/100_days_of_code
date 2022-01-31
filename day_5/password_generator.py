cd662aa3207d1881b4d9a38c8a0df5eea6d91e5a
Jennifer Baughman <jbaughman@corelogic.com>
2022-01-31 17:55:07 -0600

    Day 5: simple password generator. Random, no functions, lists, for loops.

---
+++ day_5/password_generator.py
@@ -0,0 +1,30 @@
+"""
+password_generator
+Created January 31, 2022 by Jennifer Baughman
+
+Description:
+"""
+import string
+from random import shuffle, randint, choice
+
+
+def main():
+    print("Welcome to the PyPassword Generator!")
+    len_pwd = int(input("How many letters do you want in your password? "))
+    num_symbols = int(input("How many symbols do you want? "))
+    num_digits = int(input("How many numbers do you want? "))
+    pwd_chars = []
+    alphanum = len_pwd - (num_digits + num_symbols)
+    for _ in range(num_digits):
+        pwd_chars.append(str(randint(0, 9)))
+    for _ in range(num_symbols):
+        symbols = ['!', '@', '#', '$', '%', '&']
+        pwd_chars.append(choice(symbols))
+    for _ in range(alphanum):
+        pwd_chars.append(choice(string.ascii_letters))
+    shuffle(pwd_chars)
+    print(f"Here is your password: {''.join(pwd_chars)}")
+
+
+if __name__ == "__main__":
+    main()
